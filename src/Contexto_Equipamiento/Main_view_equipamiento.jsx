5a4578ce5a991e0baf2e9b2da3ef97d315b70bba
mario-marin <mario.marin.13@sansano.usm.cl>
2020-08-04 19:55:23 -0400

    added categoria main menu

--- src/Contexto_Equipamiento/Main_view_equipamiento.jsx
+++ src/Contexto_Equipamiento/Main_view_equipamiento.jsx
@@ -7,29 +7,61 @@ export class MainEquipamiento extends Component {
 	constructor(props){
 		super(props);
 	    this.state = {
-
+	    	selector: 0
 	    };
 	    this.changeCategoria = React.createRef()
+	}
+
+	volver(){
+		this.setState({
+			selector: 0
+		});
+	}
+
+	go(value){
+		this.setState({
+			selector: value
+		});
 	}
 
 
 	render() {
 		
+		const display = {
 
-
+		}
 
 		return (
 			<div>
-				<div>
-					<AddCategoria
-						ref = {this.changeCategoria}
-						/>
-				</div>
-				<div>
+				{
+					this.state.selector == 0 ? 
+					(
+						<div>
+							<h1>Categorias</h1>
+							<div>
+								<button type="button" onClick={ () => {this.go(1)}} >Crear categoria</button>
+							</div>
+						</div>
+					) : null
+				}
+				{
+					this.state.selector == 1 ? 
+					(
+						<div>
+							<div>
+								<AddCategoria
+									ref = {this.changeCategoria}
+								/>
+							</div>
+							<div>
 
-					<button type="button" onClick={ () => {this.changeCategoria.current.save()}} >Guardar</button> 
-					<button type="button" onClick={ () => {this.enable_carousel()}} >Volver</button> 
-				</div>
+								<button type="button" onClick={ () => {this.changeCategoria.current.save()}} >Guardar</button> 
+								<button type="button" onClick={ () => {this.volver()}} >Volver</button> 
+							</div>
+						</div>
+					) : null
+				}
+				
 			</div>
 		);
 	}
