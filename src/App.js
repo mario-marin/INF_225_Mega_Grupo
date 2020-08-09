fc12c57e0a8ee772965d7bc6e1c1043e0c3ef120
Daniel <danielespinoza_@hotmail.com>
2020-08-09 16:07:30 -0400

    Comentarios instrucciones de integracion

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NewPersonal from "./Contexto_Equipo/NewPersonal";
 import EditPersonal from "./Contexto_Equipo/EditPersonal";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {MainEquipamiento} from './Contexto_Equipamiento/Main_view_equipamiento';
+// RECUERDEN HACER LOS IMPORT DE SUS COMPONENTES, DESDE SUS CARPETAS
 
 function App() {
   return (
@@ -14,12 +15,18 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
-          <Route exact path="/" component={Home} />
+          {/*Main Comun*/}
+          <Route exact path="/" component={Home} />     
           <Route path="/contexto" component={Contexto} />
+          {/*Contexto Equipo*/}
           <Route path="/personal" component={Personal} />
           <Route path="/new_personal" component={NewPersonal} />
           <Route path="/edit_personal/:id" component={EditPersonal} />
+          {/*Contexto Equipamiento*/}
           <Route path="/equipamiento" component={MainEquipamiento} />
+          {/*AQUI ABAJO PONER LAS RUTAS A SUS COMPONENTES DE LOS CONTEXTOS FALTANTES*/}
+
+
         </Switch>
       </div>
     </Router>
